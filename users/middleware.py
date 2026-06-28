6ffc219baef057feb6274feb33380f7e209805ef
kishore <kishoreml94@gmail.com>
2026-06-28 11:58:04 +0530

    Removed middleware code for temporary checks

--- users/middleware.py
+++ users/middleware.py
@@ -1,26 +1,26 @@
-from rest_framework.response import Response
-from django.http import JsonResponse
+# from rest_framework.response import Response
+# from django.http import JsonResponse
 
-def custom_auth_middleware(get_response):
-    print("Authentication Middleware Gateway ..... 🏰")
+# def custom_auth_middleware(get_response):
+#     print("Authentication Middleware Gateway ..... 🏰")
 
-    def wrapper(request):
+#     def wrapper(request):
 
-        user = request.user
+#         user = request.user
 
-        if request.path.startswith("/api/token/"):
-            response = get_response(request)
-            print(response)
-            return JsonResponse(response,status=200)
+#         if request.path.startswith("/api/token/"):
+#             response = get_response(request)
+#             print(response)
+#             return response
 
         
-        if not user.is_authenticated:
-            print("Access Blocked..... ❌❌")
-            return JsonResponse({"Unauthorized" : "This route is unauthorized. . . "},status=401) 
+#         # if not user.is_authenticated:
+#         #     print("Access Blocked..... ❌❌")
+#         #     return JsonResponse({"Unauthorized" : "This route is unauthorized. . . "},status=401) 
         
-        print("Authentication Middleware Passed . . . .✅ ✅")
+#         print("Authentication Middleware Passed . . . .✅ ✅")
 
-        response = get_response(request)
-        return response
+#         response = get_response(request)
+#         return response
     
-    return wrapper
+    #return wrapper
